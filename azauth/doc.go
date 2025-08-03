1ac2a3b34081ea155e6821b156e2752867c9f15a
Julian Koehn <me@julian.pro>
2025-08-04 00:04:09 +0200

    chore: add copyright and license information to azauth and vault package files

--- azauth/doc.go
+++ azauth/doc.go
@@ -1,3 +1,6 @@
+// Copyright (c) Kopexa GmbH
+// SPDX-License-Identifier: BUSL-1.1
+
 // Package azauth provides authentication mechanisms for Azure services.
 //
 // This package handles authentication with Azure services using various credential
--- vault/vault.go
+++ vault/vault.go
@@ -1,3 +1,6 @@
+// Copyright (c) Kopexa GmbH
+// SPDX-License-Identifier: BUSL-1.1
+
 package vault
 
 //go:generate protoc --proto_path=../../:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. --go-vtproto_out=. --go-vtproto_opt=paths=source_relative --go-vtproto_opt=features=marshal+unmarshal+size+clone vault.proto
