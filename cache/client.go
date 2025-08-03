dfd2f925349146aab1afccc8f1a7677255718989
Julian Koehn <me@julian.pro>
2025-08-03 12:31:25 +0200

    chore: add copyright and license information to cache package files

--- cache/client.go
+++ cache/client.go
@@ -1,3 +1,6 @@
+// Copyright (c) Kopexa GmbH
+// SPDX-License-Identifier: BUSL-1.1
+
 // Package cache provides a Redis client wrapper with configuration management
 // and health checking capabilities for caching operations.
 package cache
